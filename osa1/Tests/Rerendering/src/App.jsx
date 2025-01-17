f8d5e0138f373a1440e706185852e3a4a6edaeea
OliverK03 <oliver.kauppii@gmail.com>
2025-01-17 20:57:39 +0200

    part 1 tests updated

--- osa1/Tests/Rerendering/src/App.jsx
+++ osa1/Tests/Rerendering/src/App.jsx
@@ -1,8 +1,43 @@
-const App = (props) => {
-  const {counter} = props
+import { useState } from 'react'
+
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text}) => {
   return (
-    <div>{counter}</div>
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
 }
 
-export default App
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const increaseByOne = () => {
+  setCounter(counter + 1)
+  console.log('increasing, value before', counter)
+  }
+
+  const decreaseByOne = () => {
+  setCounter(counter - 1)
+  console.log('decreasing, value before', counter)
+  }
+
+  const setToZero = () => {
+  setCounter(0)
+  console.log('setting to zero, value before', counter)
+  }
+
+  return (
+    <div>
+    <Display counter={counter} />
+    <Button onClick={increaseByOne} text='plus' />
+    <Button onClick={setToZero} text='zero' />
+    <Button onClick={decreaseByOne} text='minus' />
+    </div>
+  )
+}
+
+  export default App
