cc6226308ae89896019dbf942bd6012cef5c056a
unknown <oliver.kauppii@gmail.com>
2025-02-01 17:20:59 +0200

    Part 1.7 done

--- osa1/Assignments/1.6-1.11 unicafe/src/App.jsx
+++ osa1/Assignments/1.6-1.11 unicafe/src/App.jsx
@@ -2,17 +2,41 @@ import { useState } from 'react'
 
 const App = () => {
   const [clicks, setClicks] = useState({ good: 0, neutral: 0, bad: 0})
+  const [totalClicks, setTotal] = useState(0)
+  const [score, setScore] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
+
   
   const handleGoodClick = () => {
     setClicks({ ...clicks, good: clicks.good + 1 })
+    const updateGood = clicks.good + 1
+    const newTotalClicks = updateGood + clicks.neutral + clicks.bad
+    setTotal(newTotalClicks)
+    const updateScore = score + 1
+    setScore(updateScore)
+    setAverage(updateScore / newTotalClicks)
+    setPositive(updateGood / newTotalClicks * 100)
   }
 
   const handleNeutralClick = () => {
     setClicks({ ...clicks, neutral: clicks.neutral + 1 })
+    const updateNeutral = clicks.neutral + 1
+    const newTotalClicks = clicks.good + updateNeutral + clicks.bad
+    setTotal(newTotalClicks)
+    setAverage(score / newTotalClicks)
+    setPositive(clicks.good / newTotalClicks * 100)
   }
 
   const handleBadClick = () => {
     setClicks({ ...clicks, bad: clicks.bad + 1 })
+    const updateBad = clicks.bad + 1
+    const newTotalClicks = clicks.good + clicks.neutral + updateBad
+    setTotal(newTotalClicks)
+    const updateScore = score - 1
+    setScore(updateScore)
+    setAverage(updateScore / newTotalClicks)
+    setPositive(clicks.good / newTotalClicks * 100)
   }
 
   return (
@@ -30,7 +54,10 @@ const App = () => {
         <p>
         good {clicks.good}<br />
         neutral {clicks.neutral}<br />
-        bad {clicks.bad}
+        bad {clicks.bad} <br />
+        all {totalClicks} <br />
+        average {average} <br />
+        positive {positive} % <br />
         </p>
     </div>
     </div>
