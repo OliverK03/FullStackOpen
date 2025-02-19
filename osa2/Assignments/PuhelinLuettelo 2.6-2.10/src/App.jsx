b32dbcaca25509c530e24a0fcdab73b55f9183c8
unknown <oliver.kauppii@gmail.com>
2025-02-19 14:29:34 +0200

    Part 2.8 valmis

--- osa2/Assignments/PuhelinLuettelo 2.6-2.10/src/App.jsx
+++ osa2/Assignments/PuhelinLuettelo 2.6-2.10/src/App.jsx
@@ -2,19 +2,30 @@ import { useState } from 'react'
 import Person from './components/Person'
 
 const App = (props) => {
-  const [persons, setPersons] = useState(props.persons || [])
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
 
   const handlePersonChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
   
 
   const addPerson = (event) => {
     event.preventDefault()
     const personObject = {
       name: newName,
+      number: newNumber
     }
     const ifSamename = persons.some(person => person.name === newName)
     if (ifSamename) {
@@ -24,6 +35,7 @@ const App = (props) => {
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
   return (
@@ -37,6 +49,12 @@ const App = (props) => {
         />
         </div>
         <div>
+          number: <input
+          value={newNumber}
+          onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
@@ -44,7 +62,7 @@ const App = (props) => {
       <div>
         <ul>
           {persons.map(person =>
-            <Person key={person.name} name={person.name} />
+            <Person key={person.name} name={person.name} number={person.number} />
             )}
         </ul>
       </div>
