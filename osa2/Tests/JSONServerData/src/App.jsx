3382685a7feea2aef409af539bc1c1f9ef00e0f9
unknown <oliver.kauppii@gmail.com>
2025-03-05 14:19:41 +0200

    Added .css to JSONServerData

--- osa2/Tests/JSONServerData/src/App.jsx
+++ osa2/Tests/JSONServerData/src/App.jsx
@@ -3,10 +3,26 @@ import axios from 'axios'
 import Note from './components/Note'
 import noteService from './services/notes'
 
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 16
+  }
+
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2025</em>
+    </div>
+  )
+}
+
 const App = (props) => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   const toggleImportanceOf = id => {
     const note = notes.find(n => n.id === id)
@@ -18,9 +34,12 @@ const App = (props) => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-        `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+        `Note '${note.content}' was already deleted from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -30,7 +49,7 @@ const App = (props) => {
     ? 'make not important' : 'make important'
 
     return (
-      <li>
+      <li classname='note'>
         {note.content}
         <button onClick={toggleImportance}>{label}</button>
       </li>
@@ -71,9 +90,22 @@ const App = (props) => {
       })
     }
 
+    const Notification = ({ message }) => {
+      if (message === null) {
+        return null
+      }
+
+      return (
+        <div className="error">
+          {message}
+        </div>
+      )
+    }
+
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -95,6 +127,7 @@ const App = (props) => {
         />
         <button type="submit">save</button>
       </form>
+      <Footer />
     </div>
   )
 }
